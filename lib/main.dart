bb82e035777885d359d1ae6682cd54239ca1ee00
ohanz <Chiaguguadaniel@gmail.com>
2023-09-13 03:48:46 +0100

    simp functionality

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Welcome to Revolt App',
       theme: ThemeData(
 
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Revolt App'),
     );
   }
 }
@@ -34,6 +34,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  bool show = true;
 
   void _incrementCounter() {
     //   setState(() {
@@ -60,26 +61,35 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // // Center is a layout widget. It takes a single child and positions it
-        // // in the middle of the parent.
-        // child: Column(
-        //   mainAxisAlignment: MainAxisAlignment.center,
-        //   children: <Widget>[
-        //     const Text(
-        //       'You have pushed the button this many times:',
-        //     ),
-        //     Text(
-        //       '$_counter',
-        //       style: Theme.of(context).textTheme.headlineMedium,
-        //     ),
-        //   ],
-        // ),
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            show?const Text(
+              'Observe Toggle on this Widget', style: TextStyle(fontSize: 20, color: Colors.green),
+            ): const Text(''),
+            Text(
+              'Number of Times: $_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: (){
+          setState(() {
+            _counter++;
+            if(show) {
+              show = false;
+            } else {
+              show = true;
+            }
+          });
+        },
+        tooltip: 'Show/hide',
+        child: show?const Icon(Icons.man_4_rounded):const Icon(Icons.add),
+      ), // This trailing  // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
